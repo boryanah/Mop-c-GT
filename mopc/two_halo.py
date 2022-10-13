h-units possible bug, added r200m and r200t, changed parameters, scripts for one and two-halo in a radius

mopc/two_halo.py
@@ -1,78 +1,156 @@
 import numpy as np
 from scipy.integrate import quad
-from .cosmo import rho_cz
-from .gnfw import r200
+from .params import cosmo_params
 from hmf import MassFunction, transfer
 from colossus.lss import bias,peaks
 from colossus.cosmology import cosmology
 
-
 '''set cosmology'''
-params = {'flat': True, 'H0': 70., 'Om0': 0.25, 'Ob0': 0.044, 'sigma8': 0.8159, 'ns': 0.97}
+#params = {'flat': True, 'H0': 70., 'Om0': 0.25, 'Ob0': 0.044, 'sigma8': 0.8159, 'ns': 0.97}
 
-'''parameters used in Illustris TNG https://arxiv.org/pdf/1703.02970.pdf'''
-#params = {'flat': True, 'H0': 67.7, 'Om0': 0.31, 'Ob0': 0.0486, 'sigma8': 0.8159, 'ns': 0.97}
+'''parameters used in IllustrisTNG https://arxiv.org/pdf/1703.02970.pdf'''
+params = {'flat': True, 'H0': cosmo_params['hh']*100., 'Om0': cosmo_params['Omega_m'], 'Ob0': cosmo_params['Omega_b'], 'sigma8': cosmo_params['ns'], 'ns': cosmo_params['ns']}
 
 cosmology.setCosmology('myCosmo',params)
 
-hh=0.7
-fb = params['Ob0']/params['Om0']
+# constants
 G_cgs = 6.67259e-8 #cm3/g/s2
-rhocrit =  1.87847e-29 * hh**2
-Msol_cgs = 1.989e33
-kpc_cgs = 3.086e21
+Msol_cgs = 1.989e33 #g
+kpc_cgs = 3.086e21 #cm
+
+# cosmo params
+hh = cosmo_params['hh']
+fb = params['Ob0']/params['Om0']
+Om0 = params['Om0']
 
 #################################################################################
-# Computing the 2-halo component of density and preassure profiles in cgs units #
+# Computing the 2-halo component of density and pressure profiles in cgs units #
 #################################################################################
+
+def rho_cz(z):
+    '''critical density in cgs
+    '''
+    Ez2 = cosmo_params['Omega_m']*(1+z)**3. + (1-cosmo_params['Omega_m'])
+    return cosmo_params['rhoc_0'] * cosmo_params['hh']**2. * Ez2
+
+def rho_mz(z):
+    '''mean density in cgs
+    '''
+    Mz2 = cosmo_params['Omega_m']*(1+z)**3.
+    return cosmo_params['rhoc_0'] * cosmo_params['hh']**2. * Mz2
+
+def r200crit(M, z):
+    '''radius of a sphere with density 200 times the critical density of the universe.
+    Input mass in solar masses. Output radius in cm. 
+    '''
+    M_cgs = M*Msol_cgs
+    ans = (3 * M_cgs / (4 * np.pi * 200.*rho_cz(z)))**(1.0/3.0)
+    return ans
+
+def r_virial(M, z):
+    '''radius of a sphere with density 200 times the critical density of the universe.
+    Input mass in solar masses. Output radius in cm. 
+    '''
+    M_cgs = M*Msol_cgs
+    x = cosmo_params['Omega_m']*(1.+z)**3/(cosmo_params['Omega_m']*(1.+z)**3 + (1.-cosmo_params['Omega_m'])) - 1. #Omega_m(z) - 1
+    Delta_c = 18.*(np.pi**2) + 82.*x - 39.*x*x # virial overdensity wrt critical density
+    #Delta_m = Delta_c/Omega_m(z) # wrt mean density; Delta_m = 200/0.3 = 600m so smaller than 200m
+    ans = (3 * M_cgs / (4 * np.pi * Delta_c*rho_cz(z)))**(1.0/3.0)
+    return ans
+
+def r200mean(M, z):
+    '''radius of a sphere with density 200 times the critical density of the universe.
+    Input mass in solar masses. Output radius in cm. 
+    '''
+    M_cgs = M*Msol_cgs
+    ans = (3 * M_cgs / (4 * np.pi * 200.*rho_mz(z)))**(1.0/3.0)
+    return ans
+
+def r200c_kpc(M, z):
+    '''radius of a sphere with density 200 times the critical density of the universe.
+    Input mass in solar masses. Output radius in kpc. # B.H. added
+    '''
+    ans = r200crit(M, z) # cm
+    ans /=  kpc_cgs
+    return ans
+
+def r200m_kpc(M, z):
+    '''radius of a sphere with density 200 times the matter density of the universe.
+    Input mass in solar masses. Output radius in kpc. # B.H. added
+    '''
+    ans = r200mean(M, z) # cm
+    ans /=  kpc_cgs
+    return ans
+
+def r_vir_kpc(M, z):
+    '''radius of a sphere with density 200 times the matter density of the universe.
+    Input mass in solar masses. Output radius in kpc. # B.H. added
+    '''
+    ans = r_virial(M, z) # cm
+    ans /=  kpc_cgs
+    return ans
 
 #From Battaglia 2016, Appendix A
 def rho_gnfw(x,m,z):
+    rho200c = rho_cz(z)*fb
     rho0 = 4e3 * (m/1e14)**0.29 * (1+z)**(-0.66)
     al = 0.88 * (m/1e14)**(-0.03) * (1+z)**0.19
     bt = 3.83 * (m/1e14)**0.04 * (1+z)**(-0.025)
     xc = 0.5
     gm = -0.2
     ans = rho0 * (x/xc)**gm * (1+(x/xc)**al)**(-(bt-gm)/al)
-    ans *= rho_cz(z)*fb
+    ans *= rho200c
     return ans
 
 def rhoFourier(k, m, z):
     ans = []
     for i in range(len(m)):
-        r200c = r200(m[i],z)/kpc_cgs/1e3
+        r200c = r200crit(m[i],z)/kpc_cgs/1e3
+        rvir = r_virial(m[i],z)/kpc_cgs/1e3
         integrand = lambda r: 4.*np.pi*r**2*rho_gnfw(r/r200c,m[i],z) * np.sin(k * r)/(k*r)
-        res = quad(integrand, 0., 10*r200c, epsabs=0.0, epsrel=1.e-4, limit=10000)[0]
+        res = quad(integrand, 0., 10*rvir, epsabs=0.0, epsrel=1.e-4, limit=10000)[0] # 10 times r200c originally B.H.
         ans.append(res)
     ans = np.array(ans)
     return ans
 
-def hmf(m,z):
+def hmf(m,z): # B.H. could be improved
     '''Shet, Mo &  Tormen 2001'''
-    Mmin = np.log10(np.min(m))
-    Mmax = np.log10(np.max(m))
-    return MassFunction(z=z,Mmin=Mmin, Mmax=Mmax, dlog10m=(Mmax-Mmin)/49.5, hmf_model="SMT").dndm
-
-def b(m,z):
+    Mmin = np.log10(np.min(m) * cosmo_params['hh'])
+    Mmax = np.log10(np.max(m) * cosmo_params['hh'])
+    # expects h units (returns natively h^4 Msun^-1 Mpc^3
+    return MassFunction(z=z, Mmin=Mmin, Mmax=Mmax, dlog10m=(Mmax-Mmin)/49.5, hmf_model="Behroozi").dndm * cosmo_params['hh']**4. #"SMT").dndm # "Tinker08"
+    
+    
+def b(m,z): # B.H. could be improved
     '''Shet, Mo &  Tormen 2001'''
+    """
     nu = peaks.peakHeight(m, z)
     delta_c = peaks.collapseOverdensity(corrections=True, z=z)
     aa, bb, cc = 0.707, 0.5, 0.6
     return 1.+ 1./(np.sqrt(aa)*delta_c) * (np.sqrt(aa)*aa*nu**2 + np.sqrt(aa)*bb*(aa*nu**2)**(1-cc) - ((aa*nu**2)**cc/(aa*nu**2)**cc+bb*(1-cc)*(1-cc/2)))
+    """
+    # expects h units
+    b = bias.haloBias(m*cosmo_params['hh'], model='tinker10', z=z, mdef='200c')
+    return b
 
-def Plin(k,z):
-    lnk_min = np.log(np.min(k))
-    lnk_max = np.log(np.max(k))
+def Plin(k,z): # B.H. could be improved
+    lnk_min = np.log(np.min(k) / cosmo_params['hh'])
+    lnk_max = np.log(np.max(k) / cosmo_params['hh'])
     dlnk = (lnk_max-lnk_min)/(49.5)
     '''Eisenstein & Hu (1998)'''
-    p = transfer.Transfer(sigma_8=0.8344, n=0.9624, z=z, lnk_min=lnk_min, lnk_max=lnk_max, dlnk=dlnk, transfer_model="EH")
-    return p.power
+    # expects h units
+    p = transfer.Transfer(sigma_8=params['sigma8'], n=params['ns'], z=z, lnk_min=lnk_min, lnk_max=lnk_max, dlnk=dlnk, transfer_model="CAMB")#"EH")
+    #power = np.exp(p.power)/cosmo_params['hh']**3 # acc to docs but wrong
+    power = p.power/cosmo_params['hh']**3 # Mpc^3
+    print("linear")
+    #power = p.nonlinear_power/cosmo_params['hh']**3 # Mpc^3 # overshoots
+    return power
 
 def rho_2h(r,m,z):
 
     #first compute P_2h (power spectrum)
-    m_array = np.logspace(np.log10(1.e10), np.log10(1.e15), 50, 10.)
-    k_array = np.logspace(np.log10(1.e-3), np.log10(1.e3), 50, 10.)
+    m_array = np.logspace(np.log10(1.e10), np.log10(1.e15), 50, 10.) # Msun
+    k_array = np.logspace(np.log10(1.e-3), np.log10(1.e3), 50, 10.) # 1/Mpc
     hmf_array = np.array([hmf(m_array,z)]*len(k_array)).reshape(len(k_array),len(hmf(m_array,z)))
     bias_array = np.array([b(m_array,z)]*len(k_array)).reshape(len(k_array),len(b(m_array,z)))
 
@@ -83,7 +161,7 @@ def rho_2h(r,m,z):
     P2h = np.array(arr * b(m,z)  * Plin(k_array,z))
 
     #then Fourier transform to get rho_2h
-    rcorr = 50. #Mpc/h
+    rcorr = 50. #Mpc/h # B.H. need to do something more elegant
     integrand = lambda k: 1./(2*np.pi**2.) * k**2 * np.sin(k*r)/(k*r) * np.interp(k, k_array, P2h) if k>1./rcorr else 0.0
     res = quad(integrand, 0.0, np.inf, epsabs=0.0, epsrel=1.e-2, limit=1000)[0]
     return res
@@ -92,7 +170,8 @@ def rho_2h(r,m,z):
 
 #From Battaglia 2012, AGN Feedback Delta=200
 def Pth_gnfw(x,m,z):
-    P200c = G_cgs * m*Msol_cgs * 200. * rho_cz(z) * fb /(2.*r200(m,z))
+    # B.H. I've changed all the 200 to be with respect to matter and not critical (why is this fb)
+    P200c = G_cgs * m*Msol_cgs * 200. * rho_cz(z) * fb /(2.*r200crit(m,z))
     P0 = 18.1 * (m/1e14)**0.154 * (1+z)**(-0.758)
     al = 1.0
     bt = 4.35 * (m/1e14)**0.0393 * (1+z)**0.415
@@ -105,17 +184,18 @@ def Pth_gnfw(x,m,z):
 def PthFourier(k, m, z):
     ans = []
     for i in range(len(m)):
-        r200c = r200(m[i],z)/kpc_cgs/1e3
+        r200c = r200crit(m[i],z)/kpc_cgs/1e3
+        rvir = r_virial(m[i],z)/kpc_cgs/1e3
         integrand = lambda r: 4.*np.pi*r**2*Pth_gnfw(r/r200c,m[i],z) * np.sin(k * r)/(k*r)
-        res = quad(integrand, 0., 10*r200c, epsabs=0.0, epsrel=1.e-4, limit=10000)[0]
+        res = quad(integrand, 0., 10*rvir, epsabs=0.0, epsrel=1.e-4, limit=10000)[0] # 10 times r200c originally # B.H.
         ans.append(res)
     ans = np.array(ans)
     return ans
 
 def Pth_2h(r, m, z):
     #first compute P_2h (power spectrum)
-    m_array = np.logspace(np.log10(1.e10), np.log10(1.e15), 50, 10.)
-    k_array = np.logspace(np.log10(1.e-3), np.log10(1.e3), 50, 10.)
+    m_array = np.logspace(np.log10(1.e10), np.log10(1.e15), 50, 10.) # Msun
+    k_array = np.logspace(np.log10(1.e-3), np.log10(1.e3), 50, 10.) # 1/Mpc
     hmf_array = np.array([hmf(m_array,z)]*len(k_array)).reshape(len(k_array),len(hmf(m_array,z)))
     bias_array = np.array([b(m_array,z)]*len(k_array)).reshape(len(k_array),len(b(m_array,z)))
 
@@ -125,9 +205,9 @@ def Pth_2h(r, m, z):
     arr = np.array(arr)
     P2h = np.array(arr * b(m,z)  * Plin(k_array,z))
 
-    #then Fourier transform to get Pth_2h
-    rcorr = 50. #Mpc/h
-    integrand = lambda k: 1./(2*np.pi**2.) * k**2 * np.sin(k*r)/(k*r) * np.interp(k, k_array, P2h) if k>1./rcorr else 0.0
+    #then Fourier transform to get Pth_2h # B.H. shouldn't be there
+    #rcorr = 50. #Mpc/h
+    integrand = lambda k: 1./(2*np.pi**2.) * k**2 * np.sin(k*r)/(k*r) * np.interp(k, k_array, P2h) # if k>1./rcorr else 0.0
     res = quad(integrand, 0.0, np.inf, epsabs=0.0, epsrel=1.e-2, limit=1000)[0]
     return res
 